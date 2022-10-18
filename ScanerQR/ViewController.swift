9b29f68c6f9d5ccfd0710ac1af8550df2c677b24
Eяw!и Gontier <ierwinx@me.com>
2022-10-18 11:12:43 -0500

    Initial Commit

---
+++ ScanerQR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScanerQR
+//
+//  Created by Erwin Luz León on 18/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
